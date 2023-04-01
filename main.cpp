5125b28d4f0fb984a452c1cd060dcd21dc3c3795
Blaj Matei <mateiblaj7@gmail.com>
2023-04-01 15:35:20 +0300

    Add user class and welcome message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 
+class user
+{
+private:
+    string firstName;
+    string lastName;
+    int ordersNumber;    
+public:
+    string getName();
+    user(string,string);
+    ~user();
+};
+
+user::user(string firstName, string lastName)
+{
+    this->firstName=firstName;
+    this->lastName=lastName;
+    ordersNumber=0;
+}
+
+user::~user()
+{
+}
+
+string user::getName(){
+    return this->firstName + " " + this->lastName;
+}
+
 int main(){
-    cout<<"This is [[food ordering app]]";
+    user appUser = user("John","Doe");
+    cout<<"This is [[food ordering app]]; Hello "<<appUser.getName();
 }
